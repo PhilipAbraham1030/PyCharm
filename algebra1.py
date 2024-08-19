f07c69bab6cdc0fa25edcb7dec6ce5697809c4eb
PhilipAbraham1030 <philipabraham1030@gmail.com>
2024-08-19 17:11:13 -0500

    My project first commit

---
+++ algebra1.py
@@ -0,0 +1,10 @@
+def basicfunc(x, y):
+    print(x + y)
+
+
+basicfunc(1, 2)
+# Original code
+mass = 10
+acceleration = 5
+force = mass * acceleration
+print(f"The force is {force}")
---
+++ main.py
@@ -0,0 +1,6 @@
+# Original code
+# TODO: Fix Bug
+mass = 10
+acceleration = 5
+force = mass * acceleration
+print(f"The force is {force}")
