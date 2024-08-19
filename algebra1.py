48511af9b4f3d0936a463363433ce8c1af634213
PhilipAbraham1030 <philipabraham1030@gmail.com>
2024-08-19 17:34:56 -0500

    my branch commit1

--- algebra1.py
+++ algebra1.py
@@ -1,10 +1,11 @@
-def basicfunc(x, y):
-    print(x + y)
-
-
-basicfunc(1, 2)
 # Original code
 mass = 10
 acceleration = 5
 force = mass * acceleration
+print(f"The force is {force}")
+
+# new code
+mass = 10
+acceleration = 5
+force = mass / acceleration
 print(f"The force is {force}")
